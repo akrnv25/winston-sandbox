ae88ce5e92ba4f04e11b29411977b941c0ff60f8
akrnv25 <akrnv25@gmail.com>
2023-02-21 00:43:19 +0400

    implemented LoggerService

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  logger: {
+    level: 'base',
+    fileName: './logs/combined.log',
+    writeToFile: true,
+    service: 'winston-sandbox',
+    wideFormatting: true
+  }
+};
--- src/services/logger.service.js
+++ src/services/logger.service.js
@@ -1,28 +1,90 @@
 // https://github.com/winstonjs/winston/blob/master/README.md
 
 const winston = require('winston');
+const jsonStringify = require('fast-safe-stringify');
+const { format } = require('util');
+const { combine, timestamp, printf, label } = winston.format;
+const config = require('../config');
 
 class LoggerService {
   constructor() {
-    this._logger = winston.createLogger({
-      level: 'warn',
-      format: winston.format.json(),
-      defaultMeta: { service: 'winston-sandbox' },
-      transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: './logs/warn.log', level: 'warn' }),
-        new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: './logs/combined.log' })
-      ]
+    const levels = {
+      error: 0,
+      warn: 1,
+      info: 2,
+      success: 3,
+      highlight: 4,
+      debug: 5,
+      base: 6
+    };
+
+    const colors = {
+      error: '\u001b[1;31m',
+      warn: '\u001b[1;35m',
+      info: '\u001b[1;36m',
+      success: '\u001b[1;32m',
+      highlight: '\x1b[7m\u001b[1;33m',
+      debug: '\u001b[1;33m',
+      log: '\u001B[37m'
+    };
+
+    // improved from https://github.com/winstonjs/winston/issues/1427 nikitamendelbaum commented on Nov 21, 2019
+    const myFormat = printf(
+      ({ level, message, label, timestamp, [Symbol.for('splat')]: args = [] }) => {
+        // let msg = `${timestamp} [${level}] : ${message} `;
+        // let msg = `${colors[level] || ''}[${label.toUpperCase()}]\x1b[0m | ${timestamp} | ${colors[level] || ''}${JSON.stringify(message)}\x1b[0m `;
+        let msg = `${colors[level] || ''}[${label.toUpperCase()}]\x1b[0m | ${timestamp} | ${
+          colors[level] || ''
+        }${format(message)}\x1b[0m`;
+        if (args) {
+          msg += ', ';
+          msg += format(args);
+        }
+        return msg;
+      }
+    );
+
+    // make formatting like console.log()
+    // https://github.com/winstonjs/winston/issues/1427 pgorecki commented on Jul 4, 2019
+    const logLikeFormat = {
+      transform(info) {
+        const { timestamp, label, message } = info;
+        const level = info[Symbol.for('level')];
+        const args = info[Symbol.for('splat')];
+        const strArgs = args ? args.map(jsonStringify).join(' ') : '';
+        info[Symbol.for('message')] = `${
+          colors[level] || ''
+        }[${label.toUpperCase()}]\x1b[0m | ${timestamp} | ${colors[level] || ''}${jsonStringify(
+          message
+        )}\x1b[0m ${strArgs}`;
+        return info;
+      }
+    };
+
+    const transports = [new winston.transports.Console()];
+    if (config.logger.writeToFile) {
+      const fileTransport = new winston.transports.File({
+        filename: config.logger.fileName,
+        level: config.logger.level
+      });
+      transports.push(fileTransport);
+    }
+
+    const logger = winston.createLogger({
+      colorize: true,
+      prettyPrint: true,
+      levels: levels,
+      level: config.logger.level,
+      format: combine(
+        label({ label: config.logger.service }),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        config.logger.wideFormatting === true ? myFormat : logLikeFormat
+      ),
+      transports: transports
     });
-  }
 
-  error(message) {
-    this._logger.error(message);
-  }
-
-  warn(message) {
-    this._logger.warn(message);
+    const levelKeys = Object.keys(levels);
+    levelKeys.forEach(levelKey => (this[levelKey] = logger[levelKey].bind(logger)));
   }
 }
 
